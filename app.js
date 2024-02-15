5871d6876d9f098f6ba6a329f40bb41563693500
Bamburic <michalbamburski@gmail.com>
2024-02-15 22:21:13 +0100

    style for image

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const faqContainer = document.getElementsByClassName('faq-container');
 const faqElements = document.querySelectorAll('.faq-element')
 
 for (faqPosition of faqElements){
+    // faqElement.classList.remove('active');
     faqPosition.addEventListener('click', function(){
         console.log('clicked');
         this.classList.toggle('active')
